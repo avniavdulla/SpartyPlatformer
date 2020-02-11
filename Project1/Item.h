e1dede34d76a2a5609114327d2dee2310c6dabee
Hugh Wilson <wils1486@msu.edu>
2020-02-11 00:24:46 -0500

    Base class for CItem and addition of level menu to the window w/ event handlers.

---
+++ Project1/Item.h
@@ -0,0 +1,24 @@
+/**
+ * \file Item.h
+ *
+ * \author Hugh Wilson
+ *
+ * Class for items to be used in the game
+ */
+
+#pragma once
+class CItem
+{
+	///Constructor - Uses the game it is part of 
+	///Game doesn't exist rn but this should be fine once it does
+	CItem(CGame* game);
+
+	///Default Constructor Disabled
+	CItem() = delete;
+	///Copy Constructor Disabled
+	CItem(const CItem&) = delete;
+
+private:
+	CGame mGame;
+};
+
