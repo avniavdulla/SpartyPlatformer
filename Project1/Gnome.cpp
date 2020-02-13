4a03f18100b8bb29b64298ebd94bcbd1baa4a054
Avni Avdulla <avdullaa@msu.edu>
2020-02-13 12:45:41 -0500

    Added empty Gnome class, inhereted from CItem

---
+++ Project1/Gnome.cpp
@@ -0,0 +1,7 @@
+/** 
+* \file Gnome.cpp
+* \author Avni Avdulla
+*/
+
+#include "pch.h"
+#include "Gnome.h"
---
+++ Project1/Gnome.h
@@ -0,0 +1,12 @@
+/**
+* \file Gnome.h
+* \author Avni Avdulla
+*/
+
+#pragma once
+#include "Item.h"
+class CGnome :
+	public CItem
+{
+};
+
